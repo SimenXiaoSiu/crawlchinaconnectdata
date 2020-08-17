9c8aca6032c2ed1295b5a93f6ea197e782f3efc0
Simen SS <developersiu2005@gmail.com>
2020-08-18 07:46:32 +0800

    added hkex

--- postscrape/spiders/hkex_period_spider.py
+++ postscrape/spiders/hkex_period_spider.py
@@ -10,14 +10,18 @@ class HkexshSpider(scrapy.Spider):
 
     start_urls = [
         'https://www.hkexnews.hk/sdw/search/mutualmarket_c.aspx?t=sh',
-        'https://www.hkexnews.hk/sdw/search/mutualmarket_c.aspx?t=sz'
-        #'https://www.hkexnews.hk/sdw/search/mutualmarket_c.aspx?t=hk'
+        'https://www.hkexnews.hk/sdw/search/mutualmarket_c.aspx?t=sz',
+        'https://www.hkexnews.hk/sdw/search/mutualmarket_c.aspx?t=hk'
     ]
 
     def parse(self, response):
         searchdate = response.xpath('//*[@id="pnlResult"]/h2/span/text()')[0].extract().split(' ')[1].replace('/','')
+        strtodate = datetime.strptime(searchdate, '%Y%m%d')        
         market = response.url.split('/')[-1].split('=')[1]
         for row in response.xpath('//*[@id="mutualmarket-result"]//tbody//tr'):
+            if strtodate.isoweekday() == 6:
+                continue
+
             if row.css('.col-stock-code div::text') is not None:
                 item = StockItem()
                 item['date'] = searchdate
@@ -25,12 +29,16 @@ class HkexshSpider(scrapy.Spider):
                 item['stockcode'] = row.css('.col-stock-code div::text')[1].extract()
                 item['stockname'] =  row.css('.col-stock-name div::text')[1].extract()
                 item['shareholding'] =  row.css('.col-shareholding div::text')[1].extract()
-                item['shareholdingpercent'] =  row.css('.col-shareholding-percent div::text')[1].extract()
+                #item['shareholdingpercent'] =  row.css('.col-shareholding-percent div::text')[1].extract()
+                if len(row.css('.col-shareholding-percent div::text')) >= 2:
+                    item['shareholdingpercent'] =  row.css('.col-shareholding-percent div::text')[1].extract()
+                else:
+                    item['shareholdingpercent'] = '0%'
                 yield item
         
         parsedate = (datetime.strptime(searchdate, '%Y%m%d') - timedelta(1)).strftime('%Y/%m/%d')
 
-        if parsedate >= '2020/07/02':
+        if parsedate >= '2020/08/03':
             print(parsedate)
             yield scrapy.FormRequest(
                 url = response.url,
@@ -49,8 +57,12 @@ class HkexshSpider(scrapy.Spider):
 
     def after_post(self, response):
         searchdate = response.xpath('//*[@id="pnlResult"]/h2/span/text()')[0].extract().split(' ')[1].replace('/','')
+        strtodate = datetime.strptime(searchdate, '%Y%m%d')
         market = response.url.split('/')[-1].split('=')[1]
         for row in response.xpath('//*[@id="mutualmarket-result"]//tbody//tr'):
+            if strtodate.isoweekday() == 6:
+                continue
+
             if row.css('.col-stock-code div::text') is not None:
                 item = StockItem()
                 item['date'] = searchdate
@@ -58,6 +70,10 @@ class HkexshSpider(scrapy.Spider):
                 item['stockcode'] = row.css('.col-stock-code div::text')[1].extract()
                 item['stockname'] =  row.css('.col-stock-name div::text')[1].extract()
                 item['shareholding'] =  row.css('.col-shareholding div::text')[1].extract()
-                item['shareholdingpercent'] =  row.css('.col-shareholding-percent div::text')[1].extract()
+                #item['shareholdingpercent'] =  row.css('.col-shareholding-percent div::text')[1].extract()
+                if len(row.css('.col-shareholding-percent div::text')) >= 2:
+                    item['shareholdingpercent'] =  row.css('.col-shareholding-percent div::text')[1].extract()
+                else:
+                    item['shareholdingpercent'] =  '0%'
                 yield item
 
